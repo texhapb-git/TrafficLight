0ff528747b446afd2db8d1a13841282f498ad473
TexHaPb <texhapb@bk.ru>
2020-10-23 22:12:27 +0300

    Made color changing. Added app icon. Added screenshots.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentLightColor:Int = 0
+        
+        override var prefersStatusBarHidden: Bool {
+            return true
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            updateView()
+        }
+
+        fileprivate func updateView() {
+            
+            switch currentLightColor {
+
+                case 0:
+                    view.backgroundColor = .red
+                case 1:
+                    view.backgroundColor = .yellow
+                case 2:
+                    view.backgroundColor = .green
+                default:
+                    view.backgroundColor = .red
+                    
+            }
+
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            
+            currentLightColor = currentLightColor < 2 ? currentLightColor + 1 : 0
+            updateView()
+            
+        }
 
 
 }
